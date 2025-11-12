c89b712c3fb4e33a6df429b30243308480134fcf
nrg-positron <andonbreitenfeld@utexas.edu>
2025-11-12 12:29:44 -0600

    Debugging setup.py colcon build errors

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+from glob import glob
+
+package_name = 'spot_tennis_demo'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(),  # <— change
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'maps'), glob('maps/*')),
+        (os.path.join('share', package_name, 'models'), glob('models/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Andon Breitenfeld',
+    maintainer_email='andonbreitenfeld@utexas.edu',
+    description='Demo for Spot using Nav2 and YOLO detection — navigate and localize tennis balls.',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            # add more scripts later
+            'object_pose_3d = spot_tennis_demo.object_pose_3d:main',
+            'bt_executor   = spot_tennis_demo.bt_executor:main',
+        ],
+    },
+)
