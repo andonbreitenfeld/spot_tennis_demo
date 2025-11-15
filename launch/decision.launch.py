360c78e3e50d30131920786123617c0c92cb1f69
nrg-positron <andonbreitenfeld@utexas.edu>
2025-11-15 16:39:10 -0600

    Removed bt from launch

--- launch/decision.launch.py
+++ launch/decision.launch.py
@@ -4,20 +4,6 @@ from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    bt_executor = Node(
-        package='spot_tennis_demo',
-        executable='bt_executor',
-        name='bt_executor',
-        parameters=[{
-            'bt_xml_file': PathJoinSubstitution([
-                FindPackageShare('spot_tennis_demo'),
-                'config',
-                'bt_tree.xml',
-            ]),
-        }],
-        output='screen',
-    )
-
     nav_manager = Node(
         package='spot_tennis_demo',
         executable='nav_manager',
@@ -26,6 +12,6 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        bt_executor,
+
         nav_manager,
     ])
