2ea688ce1b89c881c6b649b81de335e7cac47df9
nrg-positron <andonbreitenfeld@utexas.edu>
2025-11-17 14:48:14 -0600

    Reformatted to match John's apriltag yaml

--- launch/decision.launch.py
+++ launch/decision.launch.py
@@ -4,22 +4,25 @@ from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    # AprilTag detector (hand camera)
+    apriltag_config = PathJoinSubstitution([
+        FindPackageShare('spot_tennis_demo'),
+        'config',
+        'apriltag_settings.yaml'
+    ])
+    
     apriltag_node = Node(
         package='apriltag_ros',
         executable='apriltag_node',
         name='apriltag',
+        parameters=[
+            {'use_sim_time': False},
+            apriltag_config
+        ],
         remappings=[
-            ('image', '/spot_image_server/rgb/hand_rgb/image'),
+            ('image_rect', '/spot_image_server/rgb/hand_rgb/image'),
             ('camera_info', '/spot_image_server/rgb/hand_rgb/camera_info'),
         ],
-        parameters=[
-            PathJoinSubstitution([
-                FindPackageShare('spot_tennis_demo'),
-                'config',
-                'apriltag_settings.yaml'
-            ])
-        ]
+        emulate_tty=True,
     )
     
     ball_selector = Node(
