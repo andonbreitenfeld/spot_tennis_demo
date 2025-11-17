904d2f21192cbce8ee9ee755ce0249361d0a9b68
nrg-positron <andonbreitenfeld@utexas.edu>
2025-11-17 13:03:17 -0600

    Apriltag logic added

--- launch/decision.launch.py
+++ launch/decision.launch.py
@@ -1,7 +1,27 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
+    # AprilTag detector (hand camera)
+    apriltag_node = Node(
+        package='apriltag_ros',
+        executable='apriltag_node',
+        name='apriltag',
+        remappings=[
+            ('image', '/spot_image_server/rgb/hand_rgb/image'),
+            ('camera_info', '/spot_image_server/rgb/hand_rgb/camera_info'),
+        ],
+        parameters=[
+            PathJoinSubstitution([
+                FindPackageShare('spot_tennis_demo'),
+                'config',
+                'apriltag_settings.yaml'
+            ])
+        ]
+    )
+    
     ball_selector = Node(
         package='spot_tennis_demo',
         executable='ball_selector',
@@ -15,8 +35,17 @@ def generate_launch_description():
         name='nav_manager',
         output='screen',
     )
+    
+    bin_detector = Node(
+        package='spot_tennis_demo',
+        executable='bin_detector',
+        name='bin_detector',
+        output='screen',
+    )
 
     return LaunchDescription([
+        apriltag_node,
         ball_selector,
         nav_manager,
+        bin_detector,
     ])
